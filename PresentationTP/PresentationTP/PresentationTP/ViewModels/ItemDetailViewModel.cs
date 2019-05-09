3d037b398921147eee5ecbd8fd2810d3712934ee
prodeveloper.sidnei@outlook.com <prodeveloper.sidnei@outlook.com>
2019-05-09 12:00:26 -0300

    Learning about Xamarin...

---
+++ PresentationTP/PresentationTP/PresentationTP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PresentationTP.Models;
+
+namespace PresentationTP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
